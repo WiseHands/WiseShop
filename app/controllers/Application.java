44ecfee126415048082fb9de43a58718be7bcdb6
bogdan.tsap <bogdan.tsap@thinktank.net>
2015-08-30 19:03:16 +0300

    callback url

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,5 +12,9 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
+    public static void success(String data) {
+        System.out.println("\n\n\nApplication.success " + data);
+       ok();
+    }
 
 }
