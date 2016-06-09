d50f6fe175ab91f198a83f7f1ebe0ca807920c44
Bogdan Tsap <bohdaq@gmail.com>
2016-06-09 17:31:28 +0300

    x-auth-token login

---
+++ app/controllers/UserAPI.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import models.OrderDTO;
+import models.UserDTO;
+import play.mvc.Before;
+import play.mvc.Controller;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public class UserAPI extends Controller {
+    private static final String X_AUTH_TOKEN = "X-AUTH-TOKEN";
+
+    @Before
+    static void corsHeaders() {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Expose-Headers", "X-AUTH-TOKEN");
+    }
+
+
+    public static void register(String email, String password, String repeatPassword) throws Exception {
+        if (isValidEmailAddress(email)) {
+            UserDTO user = new UserDTO(email, password);
+            user.save();
+
+            renderJSON(json(user));
+        } else {
+            forbidden("Email Not Valid: " + email);
+        }
+    }
+
+    public static void login(String email, String password) throws Exception {
+        if (isValidEmailAddress(email)) {
+            UserDTO user = UserDTO.find("byEmail", email).first();
+
+            if(user == null)
+                forbidden("Email not found: " + email);
+
+            response.setHeader(X_AUTH_TOKEN, user.token);
+            renderJSON(json(user));
+        } else {
+            forbidden("Email Not Valid: " + email);
+        }
+    }
+
+    public static boolean isValidEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
+    }
+
+
+    private static String json(Object object){
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        return gson.toJson(object);
+    }
+
+}
---
+++ app/models/UserDTO.java
@@ -0,0 +1,35 @@
+package models;
+
+import com.google.gson.annotations.Expose;
+import enums.OrderState;
+import org.hibernate.annotations.GenericGenerator;
+import play.db.jpa.GenericModel;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class UserDTO extends GenericModel {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Expose
+    public String uuid;
+
+    @Expose
+    public String email;
+
+    @Expose
+    public String password;
+
+
+    public String token;
+
+    public UserDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.token = UUID.randomUUID().toString();
+    }
+}
