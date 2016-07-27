a56cdc5b0d23d71d4a5e108a8eeb16d34050ce68
Revsylve <patlavovach@gmail.com>
2016-07-27 17:21:13 +0300

    multiwebstore choose

--- public/js/login-app.js
+++ public/js/login-app.js
@@ -16,35 +16,47 @@
                     .success(function (data, status, headers) {
                         var token = headers("X-AUTH-TOKEN");
                         var userId = data.uuid;
-                        
+
                         if(!token || !userId){
                             $scope.deniedMsg = false;
-
                             console.error('Token or userID not returned in server response');
-
                             return;
                         }
-                        
 
+                        localStorage.setItem('X-AUTH-USER-ID', userId) ;
+                        localStorage.setItem('X-AUTH-TOKEN', token) ;
 
                         if (data.shopList.length === 1){
-                            localStorage.setItem('X-AUTH-USER-ID', userId) ;
-                            localStorage.setItem('X-AUTH-TOKEN', token) ;
-
                             var shop = data.shopList[0];
                             var domain = shop.domain;
                             window.location.href = window.location.protocol + '//' + domain + ':' + window.location.port + '/admin' +
                             '?X-AUTH-USER-ID=' + userId + "&X-AUTH-TOKEN="+token;
                         }
-                        
+                        if (data.shopList.length > 1) {
+                            $scope.showShopList = true;
+                            $scope.user = data;
+
+
+                        }
+
+
                     }).
                 error(function (data, status) {
                     console.log(JSON.stringify(data));
+
                     console.log(JSON.stringify(status));
                     $scope.deniedMsg = true;
                     $scope.accessDeniedMessage = data.status;
                 });
+
             };
+            $scope.properShop = function (shop) {
+                var domain = shop.domain;
+                var userId = localStorage.getItem('X-AUTH-USER-ID');
+                var token = localStorage.getItem('X-AUTH-TOKEN');
+                window.location.href = window.location.protocol + '//' + domain + ':' + window.location.port + '/admin' +
+                    '?X-AUTH-USER-ID=' + userId + "&X-AUTH-TOKEN="+token;
+            }
         })
 })();
 function encodeQueryData(data)
