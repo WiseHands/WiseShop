fa21b5d6980abfcf66716abe69facb161a60ff1f
Revsylve <patlavovach@gmail.com>
2016-08-01 14:48:32 +0300

    selecting active shop implemented

--- app/controllers/ShopAPI.java
+++ app/controllers/ShopAPI.java
@@ -10,8 +10,8 @@ import play.mvc.Before;
 import play.mvc.Controller;
 
 public class ShopAPI extends Controller {
-    private static final String X_AUTH_TOKEN = "X-AUTH-TOKEN";
-    private static final String X_AUTH_USER_ID = "X-AUTH-USER-ID";
+    private static final String X_AUTH_TOKEN = "x-auth-token";
+    private static final String X_AUTH_USER_ID = "x-auth-user-id";
 
     @Before
     static void corsHeaders() {
--- public/js/wise_hands/controllers/SettingsController.js
+++ public/js/wise_hands/controllers/SettingsController.js
@@ -1,5 +1,24 @@
 angular.module('WiseHands')
-    .controller('SettingsController', function ($scope, $route) {
+    .controller('SettingsController', function ($scope, $route, $http) {
         $scope.$route = $route;
+        $scope.loading = true;
+        $scope.hostName = window.location.hostname;
+        $http({
+            method: 'GET',
+            url: '/shops',
+            headers: {
+                'X-AUTH-TOKEN': localStorage.getItem('X-AUTH-TOKEN'),
+                'X-AUTH-USER-ID': localStorage.getItem('X-AUTH-USER-ID')
+            }
+        })
+            .then(function successCallback(response) {
+                $scope.loading = false;
+                $scope.shops = response.data;
+                console.log($scope.shops);
+            }, function errorCallback(data) {
+                $scope.loading = false;
+                $scope.status = 'Щось пішло не так...';
+            });
+
 
     });
