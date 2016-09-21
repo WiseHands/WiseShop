fc9df220a5fdab45037d46c552e6167880abe2c5
Revsylve <patlavovach@gmail.com>
2016-09-21 17:49:23 +0300

    transactions view

---
+++ wisehands/admin/js/controllers/TransactionsController.js
@@ -0,0 +1,20 @@
+angular.module('WiseHands')
+    .controller('TransactionsController', function ($scope, $http, signout, sideNavInit) {
+        // $scope.loading = true;
+        //
+        // $http({
+        //     method: 'GET',
+        //     url: '/shop/user',
+        //     headers: {
+        //         'X-AUTH-TOKEN': localStorage.getItem('X-AUTH-TOKEN'),
+        //         'X-AUTH-USER-ID': localStorage.getItem('X-AUTH-USER-ID')
+        //     }
+        // })
+        //     .then(function successCallback(response) {
+        //         $scope.loading = false;
+        //         $scope.users = response.data;
+        //     }, function errorCallback(data) {
+        //         $scope.loading = false;
+        //         $scope.status = 'Щось пішло не так...';
+        //     });
+    });
