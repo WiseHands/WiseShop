e3dfe4414285ee9ab33e7b132d297c78012e23b4
Revsylve <patlavovach@gmail.com>
2016-11-14 16:22:57 +0200

    superfdmin profiles

--- wisehands/superadmin/js/controllers/SideNavController.js
+++ wisehands/superadmin/js/controllers/SideNavController.js
@@ -1,7 +1,34 @@
 angular.module('SuperWiseHands')
-    .controller('SideNavController', ['$scope', '$http', '$route', '$location', 'signout',
-        function($scope, $http, $route, $location, signout) {
+    .controller('SideNavController', ['$scope', '$http', '$route', 'signout', '$window',
+        function ($scope, $http, $route, signout) {
             $scope.$route = $route;
+
+            $http({
+                method: 'GET',
+                url: '/profile',
+                headers: {
+                    'X-AUTH-TOKEN': localStorage.getItem('X-AUTH-TOKEN'),
+                    'X-AUTH-USER-ID': localStorage.getItem('X-AUTH-USER-ID')
+                }
+            })
+                .then(function successCallback(response) {
+                    $scope.profile = response.data;
+                    debugger;
+                    $scope.getProfileImage = function () {
+                        if ($scope.profile.profileUrl) {
+                            return $scope.profile.profileUrl;
+                        } else {
+                            return '/wisehands/assets/images/onerror_image/onerror_image_white.png';
+                        }
+                    };
+
+
+                }, function errorCallback(data) {
+                    console.log('error retrieving profile');
+                });
+
+
             $scope.signOut = signout.signOut;
 
-    }]);
+
+        }]);
