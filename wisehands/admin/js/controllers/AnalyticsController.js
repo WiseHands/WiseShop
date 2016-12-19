297a994316ceb261e3e024cf18a5e400b054e166
Revsylve <patlavovach@gmail.com>
2016-12-19 18:29:49 +0200

    heatmap anal

--- wisehands/admin/js/controllers/AnalyticsController.js
+++ wisehands/admin/js/controllers/AnalyticsController.js
@@ -22,26 +22,8 @@ angular.module('WiseHands')
                     }
                     $scope.labels = $scope.analytics.namesOfWeek;
                     $scope.data = $scope.analytics.totalsOfEachDay;
+
                     $scope.loading = false;
-
-                    var data = {
-                        labels: ["January", "February", "March", "April", "May", "June", "July"],
-                        datasets: [
-                            {
-                                label: "My Second dataset",
-                                fillColor: "rgba(151,187,205,0.2)",
-                                strokeColor: "rgba(151,187,205,1)",
-                                pointColor: "rgba(151,187,205,1)",
-                                pointStrokeColor: "#fff",
-                                pointHighlightFill: "#fff",
-                                pointHighlightStroke: "rgba(151,187,205,1)",
-                                data: [28, 48, 40, 19, 86, 27, 90]
-                            }
-                        ]
-                    };
-                    var ctx = document.getElementById("myChart").getContext('2d');
-                    var myLineChart = new Chart(ctx).Line(data, option);
-
                 }, function errorCallback(response) {
                     if (response.data === 'Invalid X-AUTH-TOKEN') {
                         signout.signOut();
@@ -52,38 +34,27 @@ angular.module('WiseHands')
 
             sideNavInit.sideNav();
 
-            var option = {
-                responsive: true
-            };
-            var data = {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [
-                    {
-                        label: "My Second dataset",
-                        fillColor: "rgba(151,187,205,0.2)",
-                        strokeColor: "rgba(151,187,205,1)",
-                        pointColor: "rgba(151,187,205,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: [28, 48, 40, 19, 86, 27, 90]
-                    }
-                    ,
-                    {
-                        label: "My Second dataset",
-                        fillColor: "rgba(151,187,205,0.2)",
-                        strokeColor: "rgba(151,187,205,1)",
-                        pointColor: "rgba(151,187,205,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: [28, 48, 40, 19, 86, 27, 90]
-                    }
-                ]
-            };
-            var ctx = document.getElementById("myChart").getContext('2d');
-            var myLineChart = new Chart(ctx).Line(data, option);
+// chart data example
+            var now = moment().endOf('day').toDate();
+            var yearAgo = moment().startOf('day').subtract(1, 'year').toDate();
+            var chartData = d3.time.days(yearAgo, now).map(function (dateElement) {
+                return {
+                    date: dateElement,
+                    count: (dateElement.getDay() !== 0 && dateElement.getDay() !== 6) ? Math.floor(Math.random() * 60) : Math.floor(Math.random() * 10)
+                };
+            });
+            var heatmap = calendarHeatmap()
+                .data(chartData)
+                .selector('.container')
+                .tooltipEnabled(true)
+                .colorRange(['#f4f7f7', '#79a8a9'])
+                .onClick(function (data) {
+                    console.log('data', data);
+                });
+            heatmap();  // render the chart
 
         }]);
+
+
 
 
