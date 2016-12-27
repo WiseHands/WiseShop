1f56d213f0ca131719bc83d8437a32b0da579c69
Revsylve <patlavovach@gmail.com>
2016-12-27 15:40:08 +0200

    huge shop refactoring2

--- wisehands/shop/js/controllers/CategoryDetailsController.js
+++ wisehands/shop/js/controllers/CategoryDetailsController.js
@@ -175,10 +175,3 @@ angular.module('WiseShop')
             
         }]);
 
-function encodeQueryData(data)
-{
-    var ret = [];
-    for (var d in data)
-        ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-    return ret.join("&");
-}
--- wisehands/shop/js/controllers/ProductDetailsController.js
+++ wisehands/shop/js/controllers/ProductDetailsController.js
@@ -175,11 +175,3 @@
 
 
 })();
-
-function encodeQueryData(data)
-{
-    var ret = [];
-    for (var d in data)
-        ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-    return ret.join("&");
-}
