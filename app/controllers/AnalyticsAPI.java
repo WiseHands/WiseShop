bbe66700cc80a32239de3d28018237efc4eb1b0a
Bogdan Tsap <bohdaq@gmail.com>
2017-01-10 22:07:01 +0200

    typo fix

--- app/controllers/AnalyticsAPI.java
+++ app/controllers/AnalyticsAPI.java
@@ -29,7 +29,7 @@ public class AnalyticsAPI extends AuthController {
         Long count = (Long) JPA.em().createQuery(countQuery).getSingleResult();
 
         Long today = beginOfDay(new Date());
-        String totalTodayQuery = "SELECT SUM(total) FROM OrderDTO where shop_uuid=' " + shop.uuid + "' and state!='DELETED' and state!='CANCELLED' and time > " + today;
+        String totalTodayQuery = "SELECT SUM(total) FROM OrderDTO where shop_uuid='" + shop.uuid + "' and state!='DELETED' and state!='CANCELLED' and time > " + today;
         System.out.println(totalTodayQuery);
         Double totalToday = (Double) JPA.em().createQuery(totalTodayQuery).getSingleResult();
 
