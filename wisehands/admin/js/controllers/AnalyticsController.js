c76c2e7d0d76dd5f91570d724e37e8058012ad2b
Revsylve <patlavovach@gmail.com>
2017-01-19 16:59:18 +0200

    google maps order tracking

--- wisehands/admin/js/controllers/AnalyticsController.js
+++ wisehands/admin/js/controllers/AnalyticsController.js
@@ -68,12 +68,70 @@ angular.module('WiseHands')
                     });
 
             };
+                $scope.loading = true;
+                $http({
+                    method: 'GET',
+                    url: '/orders',
+                    headers: {
+                        'X-AUTH-TOKEN': token,
+                        'X-AUTH-USER-ID': userId
+                    }
+                })
+                    .then(function successCallback(response) {
+                        $scope.orders = response.data;
+                        $scope.ordersAdresses = [];
+                        $scope.orders.forEach (function(order){
+                            if (order.destinationLat) {
+                                var lat = parseFloat(order.destinationLat);
+                                var lng = parseFloat(order.destinationLng);
+                                var latLng = [];
+                                latLng.push(lat);
+                                latLng.push(lng);
+                                $scope.ordersAdresses.push(latLng);
+                            }
+                        });
+                        initialize($scope.ordersAdresses);
+                        $scope.loading = false;
+                    }, function errorCallback(response) {
+                        if (response.data === 'Invalid X-AUTH-TOKEN') {
+                            signout.signOut();
+                        }
+                        $scope.status = 'Щось пішло не так...';
+                        $scope.loading = false;
+                    });
+
             $scope.getMainAnalyticsData('');
 
             sideNavInit.sideNav();
 
         }]);
 
+function initialize(latLng) {
+    var markers = latLng;
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+
+    map = new google.maps.Map(document.getElementById("map-container"), mapOptions);
+    map.setTilt(45);
+
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][0], markers[i][1]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map
+        });
+
+        map.fitBounds(bounds);
+        map.panToBounds(bounds);
+    }
+
+}
 
 
 
