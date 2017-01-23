9c206f63f31e3a56706b0a834ca9f066dd6e2dee
Bogdan Tsap <bohdaq@gmail.com>
2017-01-23 17:35:42 +0200

    newpost proxy

---
+++ app/controllers/ProxyAPI.java
@@ -0,0 +1,57 @@
+package controllers;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class ProxyAPI extends AuthController {
+    public static final String NEWPOSTNEARESTWAREHOUSE = "https://novaposhta.ua/shop/office/getnearestwarenhouse";
+    private final static String USER_AGENT = "Mozilla/5.0";
+
+    public static void nearestNewPost(String client, Double lat, Double lon) throws Exception {
+        renderJSON(sendPost(NEWPOSTNEARESTWAREHOUSE, lat, lon));
+    }
+
+    // HTTP POST request
+    private static String sendPost(String url, Double lat, Double lon) throws Exception {
+
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "x=" + lat + "&y=" + lon;
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        return response.toString();
+
+    }
+}
