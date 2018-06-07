7e2d76d984d50ef16f1b64b2f6777036aa21508b
TarasH <tarash@localhost.localdomain>
2018-06-07 21:21:59 +0300

    new deps for mail send

--- app/services/MailSenderImpl.java
+++ app/services/MailSenderImpl.java
@@ -9,43 +9,86 @@ import play.Play;
 import play.i18n.Messages;
 import play.libs.Mail;
 
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class MailSenderImpl implements MailSender {
     private static final boolean isDevEnv = Boolean.parseBoolean(Play.configuration.getProperty("dev.env"));
+    private static final String gmailUsername = Play.configuration.getProperty("mail.smtp.user");
+    private static final String gmailPass = Play.configuration.getProperty("mail.smtp.pass");
+
+    static Properties mailServerProperties;
+    static Session getMailSession;
+    static MimeMessage generateMailMessage;
 
     public void sendEmail(ShopDTO shop, OrderDTO order, String status) throws Exception {
         System.out.println("MailSenderImpl " + isDevEnv + status + shop.contact.email);
         if (!isDevEnv) {
-            HtmlEmail email = new HtmlEmail();
-            email.setHostName(shop.domain);
-            email.setFrom("wisehandsme@gmail.com");
-            System.out.println("AddTo: " + shop.contact.email);
-            email.addTo(shop.contact.email);
-            email.setSubject(status);
-            email.setHtmlMsg(order.toString());
-            Mail.send(email);
+//            HtmlEmail email = new HtmlEmail();
+//            email.setHostName(shop.domain);
+//            email.setFrom("wisehandsme@gmail.com");
+//            System.out.println("AddTo: " + shop.contact.email);
+//            email.addTo(shop.contact.email);
+//            email.setSubject(status);
+//            email.setHtmlMsg(order.toString());
+//            Mail.send(email);
+
+
+            System.out.println("\n 1st ===> setup Mail Server Properties..");
+            mailServerProperties = System.getProperties();
+            mailServerProperties.put("mail.smtp.port", "587");
+            mailServerProperties.put("mail.smtp.auth", "true");
+            mailServerProperties.put("mail.smtp.starttls.enable", "true");
+            System.out.println("Mail Server Properties have been setup successfully..");
+
+            // Step2
+            System.out.println("\n\n 2nd ===> get Mail Session..");
+            getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+            generateMailMessage = new MimeMessage(getMailSession);
+            generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(shop.contact.email));
+            generateMailMessage.setSubject(status);
+            String emailBody = order.toString();
+            generateMailMessage.setContent(emailBody, "text/html");
+            System.out.println("Mail Session has been created successfully..");
+
+            // Step3
+            System.out.println("\n\n 3rd ===> Get Session and Send mail");
+            Transport transport = getMailSession.getTransport("smtp");
+
+            // Enter your correct gmail UserID and Password
+            // if you have 2FA enabled then provide App Specific Password
+            transport.connect("smtp.gmail.com", gmailUsername, gmailPass);
+            transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+            transport.close();
         }
     }
 
     public void sendEmailToInvitedUser(ShopDTO shop, UserDTO user) throws Exception {
-        if (!isDevEnv) {
-            String loginUrl = "http://";
-            if(isDevEnv) {
-                loginUrl += "localhost:3334/";
-            } else {
-                loginUrl += "wisehands.me/";
-            }
-
-            HtmlEmail email = new HtmlEmail();
-            email.setHostName(shop.domain);
-            email.setFrom("noreply@" + shop.domain);
-            email.addTo(user.email);
-
-            String title = Messages.get("user.added.to.shop.email.title", shop.shopName);
-            email.setSubject(title);
-            String msg = Messages.get("user.added.to.shop.email", shop.shopName);
-            email.setMsg(msg);
-
-            Mail.send(email);
-        }
+//        if (!isDevEnv) {
+//            String loginUrl = "http://";
+//            if(isDevEnv) {
+//                loginUrl += "localhost:3334/";
+//            } else {
+//                loginUrl += "wisehands.me/";
+//            }
+//
+//            HtmlEmail email = new HtmlEmail();
+//            email.setHostName(shop.domain);
+//            email.setFrom("noreply@" + shop.domain);
+//            email.addTo(user.email);
+//
+//            String title = Messages.get("user.added.to.shop.email.title", shop.shopName);
+//            email.setSubject(title);
+//            String msg = Messages.get("user.added.to.shop.email", shop.shopName);
+//            email.setMsg(msg);
+//
+//            Mail.send(email);
+//        }
     }
 }
