d1badad9c7bf29c36871ce88629f920a38b302bd
TarasH <tarash@localhost.localdomain>
2018-06-07 20:13:29 +0300

    sends email to shop.contact.email on new order

--- app/controllers/OrderAPI.java
+++ app/controllers/OrderAPI.java
@@ -167,6 +167,11 @@ public class OrderAPI extends AuthController {
 
         JPA.em().getTransaction().commit();
         new SendSmsJob(order, shop).now();
+        try {
+            mailSender.sendEmail(shop, order, Messages.get("new.order"));
+        } catch (Exception e) {
+            System.out.println("OrderAPI create mail error" + e.getCause() + e.getStackTrace());
+        }
 
 //        List<PushSubscription> subscriptions = PushSubscription.find("byShopUuid", shop.uuid).fetch();
 //        for(PushSubscription subscription: subscriptions) {
