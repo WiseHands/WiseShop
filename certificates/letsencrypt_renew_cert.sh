9ef5bebcb129bbdb84aaa6c3bd1ac63a25eab82e
Taras H <research.010@gmail.com>
2018-09-18 22:30:57 +0300

    updated script paths & removed unused code

--- certificates/letsencrypt_renew_cert.sh
+++ certificates/letsencrypt_renew_cert.sh
@@ -21,19 +21,7 @@ readarray rows < /home/bogdan/wisehands/domains.txt
 for row in "${rows[@]}"; do
 row_array=(${row})
 first=${row_array[0]}
-#echo "-d ${first}"
 /usr/bin/certbot certonly --agree-tos --keep-until-expiring --email $email --webroot -w $w_root -d ${first} --post-hook="/sbin/service lighttpd reload"
-cat /etc/letsencrypt/live/${first}/privkey.pem  /etc/letsencrypt/live/${first}/cert.pem > ssl.pem
+cat /etc/letsencrypt/live/${first}/privkey.pem  /etc/letsencrypt/live/${first}/cert.pem > /etc/letsencrypt/live/${first}/ssl.pem
 done
 /sbin/service lighttpd restart
-### Test ###
-
-#for domain in ${domains[@]}; do
-#echo "-d $domain"
-#done
-
-#for domain in "${domains[@]}"; do
-#echo "/usr/bin/certbot certonly --agree-tos --keep-until-expiring --dry-run --email $email --webroot -w $w_root -d $domain --post-hook="service lighttpd reload""
-#    cat /etc/letsencrypt/live/$domain/privkey.pem  /etc/letsencrypt/live/$domain/cert.pem > ssl.pem
-    #&&service lighttpd restart in crontab
-#done
