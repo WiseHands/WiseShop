f522bb92351426734a28b8cb08772c0371c0d4c9
top802 <tatrun89@gmail.com>
2019-02-26 16:04:38 +0200

    added button my location

--- wisehands/shop/js/controllers/SelectedAddressDeliveryController.js
+++ wisehands/shop/js/controllers/SelectedAddressDeliveryController.js
@@ -67,6 +67,7 @@
                       var latlng;
                       var polygon;
                       var geocoder;
+                      var infoWindow;
                       // var isAddress;
                       $scope.buttonDisabled = true;
                       var arrayCoordinates;
@@ -84,7 +85,7 @@
                               zoom: 16
                               };
                           // set googleMap By Id
-
+                          infoWindow = new google.maps.InfoWindow;
                           map = new google.maps.Map(document.getElementById("googleMap"), var_map_options);
 
                           google.maps.event.addListener(map, 'click', function(event) {
@@ -95,10 +96,37 @@
                               geocodeLatLng(latlng, isAddress);
                             });
 
-
                         polygonMap();
+                        
+                        $scope.myLocation = function(){
+                          if (navigator.geolocation) {
+                            navigator.geolocation.getCurrentPosition(function(position) {
+                              var pos = {
+                                lat: position.coords.latitude,
+                                lng: position.coords.longitude
+                              };
 
+                              infoWindow.setPosition(pos);
+                              infoWindow.setContent('Location found.');
+                              infoWindow.open(map);
+                              map.setCenter(pos);
+                            }, function() {
+                              handleLocationError(true, infoWindow, map.getCenter());
+                            });
+                          } else {
+                            // Browser doesn't support Geolocation
+                            handleLocationError(false, infoWindow, map.getCenter());
+                          }
+                        }
                       }
+
+                      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                              infoWindow.setPosition(pos);
+                              infoWindow.setContent(browserHasGeolocation ?
+                                                    'Error: The Geolocation service failed.' :
+                                                    'Error: Your browser doesn\'t support geolocation.');
+                              infoWindow.open(map);
+                            }
 
                         function polygonMap(){
                           var objectCoordinates = [];
