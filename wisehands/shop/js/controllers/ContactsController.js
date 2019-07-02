a6f9beceef86fdd35c37f5e2b21f341eea8ad7b0
Aleksander <tatrun89@gmail.com>
2019-07-02 17:41:14 +0300

    fixed google map in contactController.js

--- wisehands/shop/js/controllers/ContactsController.js
+++ wisehands/shop/js/controllers/ContactsController.js
@@ -8,10 +8,34 @@
                 .then(function successCallback(response) {
                     $scope.contacts = response.data;
                     console.log(response);
-                    window.init_map($scope.contacts.latLng);
+                    initMap($scope.contacts.latLng);
                 }, function errorCallback(data) {
                     $scope.status = 'Щось пішло не так...';
                 });
+
+
+
+            function initMap(latLng) {
+                if (!latLng) return;
+                var map;
+                var cords = latLng.split(',');
+                var lat = cords[0];
+                var lng = cords[1];
+                var var_location = new google.maps.LatLng(lat, lng);
+                var var_map_options = {
+                    center: var_location,
+                    zoom: 15
+                };
+                var var_marker = new google.maps.Marker({
+                    position: var_location,
+                    map: map
+                });
+                // set googleMap By Id
+                map = new google.maps.Map(document.getElementById("map-container"), var_map_options);
+                var_marker.setMap(map);
+            };
+
+
 
         }]);
 
