f6c7a24929a8d3eb21c80b7f4ac4afe187314c64
Aleksander <tatrun89@gmail.com>
2019-07-09 14:59:38 +0300

    added showInfoMsg in OtherShopsController

--- wisehands/shop/js/controllers/OtherShopsController.js
+++ wisehands/shop/js/controllers/OtherShopsController.js
@@ -71,6 +71,8 @@
                             var distanceToShops = [];
                             var origin = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
+                            geocodeOriginPosition(origin);
+
                             for (var i=0; i<shopLatCoords.length; i++){
                                 for(var j=0; j<shopLngCoords.length; j++){
                                     var destination = new google.maps.LatLng(shopLatCoords[i], shopLngCoords[j]);
@@ -103,6 +105,36 @@
                         showWarningMsg('Geolocation not available')
                     }
 
+                }
+
+                function geocodeOriginPosition(latlng) {
+                    let geocoder = new google.maps.Geocoder();
+                    geocoder.geocode({
+                        'location': latlng
+                    }, function(results, status) {
+                        if (status === 'OK') {
+                            if (results[0]) {
+                                console.log('geocoding result: ', results);
+
+                                // let address = results[0].formatted_address;
+                                let newAdd = [];
+                                for (var i = 0; i<=3; i++){
+                                    let address = results[0].address_components[i];
+                                    newAdd.push(address.long_name);
+
+                                }
+                                let address = newAdd.reverse(newAdd).join().split(',').join(', ');
+                                showInfoMsg(address);
+
+                                console.log('geocoding result address: ', address);
+
+                            } else {
+                                console.log('no address');
+                            }
+                        } else {
+                            console.log('finded address ', status);
+                        }
+                    });
                 }
         }]);
 })();
