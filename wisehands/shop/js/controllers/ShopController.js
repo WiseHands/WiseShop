52c9dc47a31b9ba75d613e652b28c7ea82043a34
Aleksander <tatrun89@gmail.com>
2019-10-21 18:03:14 +0300

    add button resources "show more"

--- wisehands/shop/js/controllers/ShopController.js
+++ wisehands/shop/js/controllers/ShopController.js
@@ -84,30 +84,7 @@
                             } else {
                                 $scope.hideMoreButton = true;
                             }
-                            $scope.isAllOrdersDeleted = true;
-                            var now = new Date();
-                            var dateNow = new Date(now.getUTCFullYear(), now.getMonth(), now.getDate());
-                            var startOfToday = dateNow.getTime();
-                            var oneDayInMs = 86400000;
-                            $scope.orders.forEach(function(order){
-                                order.yesterdayString = false;
-                                if (startOfToday - oneDayInMs < order.time && startOfToday > order.time){
-                                    order.yesterdayString = true;
-                                } else if (startOfToday < order.time) {
-                                    var date = new Date(order.time);
-                                    var hour = (date.getHours()<10?'0':'') + date.getHours();
-                                    var minute = (date.getMinutes()<10?'0':'') + date.getMinutes();
-                                    order.properDate = hour + ':' + minute;
-                                } else {
-                                    var orderDate = new Date(order.time);
-                                    var orderDay = ("0" + orderDate.getDate()).slice(-2);
-                                    var orderMonth = ("0" + (orderDate.getMonth() + 1)).slice(-2);
-                                    order.properDate = orderDay + '.' + orderMonth;
-                                }
-                                if (order.state !== 'DELETED') {
-                                    $scope.isAllOrdersDeleted = false;
-                                }
-                            });
+
                             pageNumber ++;
                             $scope.loading = false;
                         }, function errorCallback(response) {
