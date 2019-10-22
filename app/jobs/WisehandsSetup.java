3f99c5909ccd639f3aedd15703156fea771f18eb
Aleksander <tatrun89@gmail.com>
2019-10-22 13:26:41 +0300

    set contract page

--- app/jobs/WisehandsSetup.java
+++ app/jobs/WisehandsSetup.java
@@ -24,6 +24,7 @@ public class WisehandsSetup extends Job {
     private static final String DOSTAVKA = "<p>Доставка доступна у таких варіантах:</p><ul>\t<li>самовивіз по адресі</li>\t<li>доставка кур&#39;єром у межах міста</li>\t<li>відправлення на відділення Нової Пошти</li></ul><p>При оформленні замовлення до вас зателефонує менеджер та уточнить усі деталі.</p>";
     private static final String POVERNENNYA = "<p>Повернення товару здійснюється відповідно до Закону України &laquo;Про захист прав споживачів&raquo;. У період 14 днів, не враховуючи дня покупки, можна оформити повернення товару за умов:</p><ul>\t<li>товар не був у експлуатації</li>\t<li>цілісність комплекту та упаковки не порушена</li>\t<li>збережений документ, який підтверджує оплату</li>\t<li>виріб не належить до переліку товарів неналежної якості, повернення яких обмежено рішенням Кабінету Міністрів України (постанова від 19 березня 1994 року №172)</li></ul><p>&nbsp;</p>";
 
+
     private static final String PASSWORD = "rjylbnth";
 
     private static final boolean isDevEnv = Boolean.parseBoolean(Play.configuration.getProperty("dev.env"));
@@ -78,12 +79,22 @@ public class WisehandsSetup extends Job {
             shop = shop.save();
 
             if (!hasPages){
+                Contract contract = new Contract();
+                String contacts =
+                        "<p>Наші контакти для Вас: </p><ul>\t<li>Телефон "
+                        + shop.contact.phone + "</li>\t<li>Емейл "
+                        + shop.contact.email + "</li>\t<li>Адреса "
+                        + shop.contact.address + "</li>\t</ul>" +
+                        "<p>&nbsp;</p>";
+
+                String aboutUs = "Про нас...";
+
                 PageConstructorDTO page = new PageConstructorDTO("/page/", "Оплата", OPLATA, shop);
                 PageConstructorDTO page1 = new PageConstructorDTO("/page/", "Доставка", DOSTAVKA, shop);
                 PageConstructorDTO page2 = new PageConstructorDTO("/page/", "Повернення", POVERNENNYA, shop);
-                PageConstructorDTO page3 = new PageConstructorDTO("/page/", "Контакти", "create page Контакти from java", shop);
-                PageConstructorDTO page4 = new PageConstructorDTO("/page/", "Про нас", "create page Про нас from java", shop);
-                PageConstructorDTO page5 = new PageConstructorDTO("/page/", "Договір", "create page Договір from java", shop);
+                PageConstructorDTO page3 = new PageConstructorDTO("/page/", "Контакти", contacts, shop);
+                PageConstructorDTO page4 = new PageConstructorDTO("/page/", "Про нас", aboutUs, shop);
+                PageConstructorDTO page5 = new PageConstructorDTO("/page/", "Договір", contract.createContract(shop), shop);
                 page.save();
                 page1.save();
                 page2.save();
