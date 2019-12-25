a03519f24a01a8aa6cffa2f9cd767fbd3af6a5a0
Aleksander <tatrun89@gmail.com>
2019-12-25 19:39:37 +0200

    analytics refactored

--- wisehands/admin/js/controllers/AnalyticsController.js
+++ wisehands/admin/js/controllers/AnalyticsController.js
@@ -1,6 +1,6 @@
 angular.module('WiseHands')
-    .controller('AnalyticsController', ['$scope', '$http', '$route', 'sideNavInit', 'signout',
-        function ($scope, $http, $route, sideNavInit, signout) {
+    .controller('AnalyticsController', ['$scope', '$http', '$route', 'sideNavInit',
+        function ($scope, $http, $route, sideNavInit) {
             $scope.loading = true;
 
             let fromDateInput = document.getElementById("fromDateForAnalytics");
@@ -21,7 +21,7 @@ angular.module('WiseHands')
                 toDateInput.value = moment(from).format(format);
 
 
-                $scope.calculateDayRange(to, from);
+                $scope.calculateDayRange(from, to);
             };
 
             function convertDateToMilissecondsWithoutTimezoneOffset(date) {
@@ -36,18 +36,14 @@ angular.module('WiseHands')
             }
 
             $scope.performRequestInGivenRange = function () {
-                let fromDateInput = document.getElementById("fromDateForAnalytics");
-                let toDateInput = document.getElementById("toDateForAnalytics");
-
                 let fromDate = convertDateToMilissecondsWithoutTimezoneOffset(fromDateInput.value);
                 let toDate = convertDateToMilissecondsWithoutTimezoneOffset(toDateInput.value);
-
                 $scope.calculateDayRange(fromDate, toDate);
             };
 
-            $scope.calculateDayRange = function(today, pastWeekDate){
-                let fromDate = pastWeekDate;
-                let toDate = today;
+            $scope.calculateDayRange = function(from, to){
+                let fromDate = from;
+                let toDate = to;
                 console.log('fromDate.value for calculateDayRange: ', fromDate);
                 console.log('toDate.value for calculateDayRange: ', toDate);
 
@@ -68,8 +64,8 @@ angular.module('WiseHands')
 
                         let arrayTime = $scope.analytics.chartData;
 
-                        fromDateInput.value = arrayTime[arrayTime.length - 1].day.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$1-$2");
-                        toDateInput.value = arrayTime[0].day.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$1-$2");
+                        toDateInput.value = arrayTime[arrayTime.length - 1].day.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$1-$2");
+                        fromDateInput.value = arrayTime[0].day.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$1-$2");
 
                       $scope.loading = false;
 
@@ -96,69 +92,16 @@ angular.module('WiseHands')
 
             };
 
-            $scope.loading = true;
-              $http({
-                 method: 'GET',
-                 url: '/orders',
-              }).then(function successCallback(response) {
-                    $scope.orders = response.data;
-                    $scope.ordersAdresses = [];
-                    $scope.orders.forEach (function(order){
-                        if (order.destinationLat) {
-                              var lat = parseFloat(order.destinationLat);
-                              var lng = parseFloat(order.destinationLng);
-                              var latLng = [];
-                              latLng.push(order.address);
-                              latLng.push(lat);
-                              latLng.push(lng);
-                              latLng.push(order.uuid);
-                              $scope.ordersAdresses.push(latLng);
-                            }
-                        });
-                        initialize($scope.ordersAdresses);
-                        console.log("$scope.ordersAdresses = []", $scope.ordersAdresses = []);
-                        $scope.loading = false;
-                 }, function errorCallback(response) {
-                        $scope.loading = false;
-              });
             let week = 7;
 
             const currentDate = new Date();
             const formattedCurrentDate = convertDateToMilissecondsWithoutTimezoneOffset(currentDate);
             const pastWeekDate = currentDate.setDate(currentDate.getDate()- week);
             const formattedPassWeekDate = convertDateToMilissecondsWithoutTimezoneOffset(pastWeekDate);
-            $scope.calculateDayRange(formattedCurrentDate, formattedPassWeekDate);
+            $scope.calculateDayRange(formattedPassWeekDate, formattedCurrentDate);
 
             sideNavInit.sideNav();
 
         }]);
 
-function initialize(latLng) {
-    var markers = latLng;
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        mapTypeId: 'roadmap'
-    };
 
-    map = new google.maps.Map(document.getElementById("map-container"), mapOptions);
-    map.setTilt(45);
-
-    for( i = 0; i < markers.length; i++ ) {
-        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: markers[i][0],
-            uuid: markers[i][3]
-        });
-
-        map.fitBounds(bounds);
-        map.panToBounds(bounds);
-
-        marker.addListener('click', function() {
-            window.location.href = 'admin#/details/' + this.uuid;
-        });
-    }
-}
