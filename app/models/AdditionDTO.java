706d82cf756177a0fc4b6905f8948097a096e831
Aleksander <tatrun89@gmail.com>
2020-02-19 18:14:57 +0200

    fixed bug related to round product and addition price

--- app/models/AdditionDTO.java
+++ app/models/AdditionDTO.java
@@ -39,4 +39,14 @@ public class AdditionDTO extends GenericModel {
     @ManyToOne
     public ProductDTO product;
 
+    public String formatDecimal() {
+        Double number = this.price;
+        float epsilon = 0.004f; // 4 tenths of a cent
+        if (Math.abs(Math.round(number) - number) < epsilon) {
+            return String.format("%10.0f", number); // sdb
+        } else {
+            return String.format("%10.2f", number); // dj_segfault
+        }
+    }
+
 }
