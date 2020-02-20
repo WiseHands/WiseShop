719653f9f35e39a627ff61cbf1fa9a89805c8ab0
Taras H <research.010@gmail.com>
2020-02-20 16:11:24 +0200

    shopping cart email and request fix

--- wisehands/polymer3/shopping-cart/build/es5-bundled/src/wise-shopping-cart-container.js
+++ wisehands/polymer3/shopping-cart/build/es5-bundled/src/wise-shopping-cart-container.js
@@ -24290,9 +24290,6 @@ class WiseShoppingCartItem extends PolymerElement {
         .image-container {
             padding: .5em;
         }
-        .image-container:hover{
-            cursor: pointer;
-        }
         iron-image {
             display: flex;
         } 
@@ -24303,14 +24300,6 @@ class WiseShoppingCartItem extends PolymerElement {
            min-width: 0;  
            flex: 1;
            justify-content: space-between;
-        }
-        .product-info-container{
-            flex-direction: column;
-        }
-        .product-info-container h4{
-           font-size: 0.8rem;
-           font-weight: 400;
-           margin: 5px 0;;
         }
         .quantity-span, .total-span {
            margin: 0 .5em;
@@ -24324,10 +24313,6 @@ class WiseShoppingCartItem extends PolymerElement {
            text-overflow: ellipsis;
            margin: 0;
            font-weight: normal;
-        }
-        
-        .total-container h3:hover {
-            cursor: pointer;
         }
        
         .product-calculated-container {
@@ -24347,27 +24332,19 @@ class WiseShoppingCartItem extends PolymerElement {
         }
       </style>
     <paper-card class="paper-card-container">
-        <div class="image-container">
-            <iron-image sizing="cover" height="100" width="100" src="[[cartItem.imagePath]]" on-click="_openProductPageByUuid">
-        </div>
-        <div class="total-container">
-            <div class="product-info-container">
-                <h3 on-click="_openProductPageByUuid">[[cartItem.name]]</h3>
-                <h4> 
-                    <template is="dom-repeat" items="[[cartItem.additionList]]">                
-                        [[item.title]]<span hidden="[[!hasMoreThanOneQuantity(item)]]">([[item.quantity]])</span>
-                        <span hidden="[[isLastItem(cartItem.additionList, index)]]">,</span>
-                    </template>        
-                </h4>
-            </div>        
-            <div class="product-calculated-container">  
+      <div class="image-container">
+        <iron-image sizing="cover" height="100" width="100" src="[[cartItem.imagePath]]">
+      </div>
+      <div class="total-container">
+        <h3>[[cartItem.name]]</h3>
+          <div class="product-calculated-container">  
                 <paper-icon-button icon="remove" on-tap="_decreaseItemQuantity"></paper-icon-button>
                 <p class="quantity-span">[[cartItem.quantity]]</p>
                 <paper-icon-button icon="add" on-tap="_increaseItemQuantity"></paper-icon-button>
-                <div class="total-span">[[_calculateTotalPrice(cartItem.quantity, cartItem.price, cartItem.additionList)]]<br>[[currencyLabel]]</div>
+                <div class="total-span">[[_calculateTotalPrice(cartItem.quantity, cartItem.price)]]<br>[[currencyLabel]]</div>
                 <paper-icon-button icon="close" on-tap="_removeItem"></paper-icon-button>
-            </div>
-        </div>
+          </div>
+      </div>
     </paper-card>
     `;
   }
@@ -24387,24 +24364,12 @@ class WiseShoppingCartItem extends PolymerElement {
     };
   }
 
-  _calculateTotalPrice(quantity, productPrice, additionList) {
-    let additionPrice = 0;
-    additionList.forEach(item => {
-      additionPrice += item.price * item.quantity;
-    });
-    return quantity * (productPrice + additionPrice);
+  _calculateTotalPrice(quantity, price) {
+    return quantity * price;
   }
 
   _decreaseItemQuantity() {
     this.dispatchEvent(new CustomEvent('decrease-item-quantity', {
-      detail: this.cartItem.uuid,
-      bubbles: true,
-      composed: true
-    }));
-  }
-
-  _openProductPageByUuid() {
-    this.dispatchEvent(new CustomEvent('open-product', {
       detail: this.cartItem.uuid,
       bubbles: true,
       composed: true
@@ -24425,14 +24390,6 @@ class WiseShoppingCartItem extends PolymerElement {
       bubbles: true,
       composed: true
     }));
-  }
-
-  isLastItem(additionList, index) {
-    return index + 1 === additionList.length;
-  }
-
-  hasMoreThanOneQuantity(item) {
-    return item.quantity > 1;
   }
 
 }
@@ -24837,6 +24794,10 @@ class WiseShoppingCartContainer extends PolymerElement {
         value: 'USD'
       },
       courierLabel: String,
+      isMakeOrderRequestRunning: {
+        type: Boolean,
+        value: false
+      },
       googleMapsApiKey: {
         type: String,
         value: 'AIzaSyAuKg9jszEEgoGfUlIqmd4n9czbQsgcYRM'
@@ -24984,13 +24945,17 @@ class WiseShoppingCartContainer extends PolymerElement {
   }
 
   _makeOrderRequest() {
+    if (this.isMakeOrderRequestRunning) return;
     const ajax = this.$.makeOrderAjax;
     ajax.url = `${this.hostname}/order${this.addCartIdParamIfAvailable(true)}`;
     ajax.method = 'POST';
+    this.isMakeOrderRequestRunning = true;
     ajax.generateRequest();
   }
 
   _onOrderResponseChanged(event, detail) {
+    console.log('order response', event, detail);
+    this.isMakeOrderRequestRunning = false;
     this.cart = {
       items: []
     };
