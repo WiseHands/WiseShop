4d06e67f055d9860cc364ec9f8ec7d02a3f9ec4b
Taras H <research.010@gmail.com>
2020-02-26 18:40:34 +0200

    shopping cart container max-width fix

--- wisehands/polymer3/shopping-cart/build/es5-bundled/service-worker.js
+++ wisehands/polymer3/shopping-cart/build/es5-bundled/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["src/index.html","0f5e868f3405ceeb38271a764177a4c5"],["src/wise-shopping-cart-container.js","9908ea78d6a38f8faedaff775e4cab99"]];
+var precacheConfig = [["src/index.html","0f5e868f3405ceeb38271a764177a4c5"],["src/wise-shopping-cart-container.js","ea76a0d66ca0cd5baaf1064c06597870"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
