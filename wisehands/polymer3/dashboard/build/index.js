3b943273c0c76bbf63a25da54da52fcbe53ae66f
Aleksander <tatrun89@gmail.com>
2020-03-18 19:41:10 +0200

    wip restyling dashboard

--- wisehands/polymer3/dashboard/build/index.js
+++ wisehands/polymer3/dashboard/build/index.js
@@ -2670,14 +2670,12 @@ class SubcriptionContainer extends LitElement {
                 }
                 .main-container{
                     display: flex;
-                    margin: 15px;
-                    width: 98%;
-                    height: 98%;
+                    margin: 15px;                   
                 }
                                      
             </style>
             
-            <div class="main-container border">
+            <div class="main-container">
               <p>Balance: ${this.balance}</p>
             </div>
 
@@ -2746,6 +2744,94 @@ class SubcriptionContainer extends LitElement {
 
 
 customElements.define('subcription-container', SubcriptionContainer);
+
+// Import the LitElement base class and html helper function
+
+class ProfileContainer extends LitElement {
+  render() {
+    return html`
+            <style>
+                .border{
+                    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .16), 0 2px 10px 0 rgba(0, 0, 0, .12);
+                }
+                .main-container{
+                    display: flex;
+                    margin: 15px;
+                    
+                }
+                                     
+            </style>
+            
+            <div class="main-container">
+              <p>Balance: ${this.balance}</p>
+            </div>
+
+    `;
+  }
+
+  static get properties() {
+    return {
+      balance: {
+        type: String,
+        value: '0.0.0.0'
+      }
+    };
+  }
+
+  constructor() {
+    super();
+    this.getUserInfo();
+    this.balance = '0.0.0.1';
+  }
+
+  _buildUrlForShop(item) {
+    const token = localStorage.getItem('JWT_TOKEN');
+    return `${window.location.protocol}//${item.domain}:${window.location.port}/admin?JWT_TOKEN=${token}`;
+  }
+
+  getShopList() {
+    const _this = this;
+
+    const url = '/api/dashboard/shops';
+    let token = localStorage.getItem('JWT_TOKEN');
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        authorization: 'Bearer ' + token
+      }
+    }).then(function (response) {
+      console.log("response response: ", response);
+      return response.json();
+    }).then(function (data) {
+      console.log('data for shopList: ', data);
+
+      if (data) {
+        _this.shopList = data;
+      }
+    });
+  }
+
+  getUserInfo() {
+
+    const url = '/api/dashboard/user';
+    let token = localStorage.getItem('JWT_TOKEN');
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        authorization: 'Bearer ' + token
+      }
+    }).then(function (response) {
+      console.log("response response: ", response);
+      return response.json();
+    }).then(function (data) {
+      console.log('data for users: ', data);
+    });
+  }
+
+} // Register the new element with the browser.
+
+
+customElements.define('profile-container', ProfileContainer);
 
 // Import the LitElement base class and html helper function
 
@@ -2829,6 +2915,9 @@ class DashBoard extends LitElement {
                     width: 75%;
                 }
                     .shop-list-container{
+                        width: 100%;
+                        height: 100%;
+                        display: flex;
                         flex-direction: row;
                         align-items: flex-start;
                         flex-wrap: wrap;
@@ -2913,12 +3002,12 @@ class DashBoard extends LitElement {
                         </div>` : html``} 
                         
                         ${this.isShowSubscriptionContainer ? html`
-                        <subcription-container></subcription-container>` : ''}
+                            <subcription-container></subcription-container>
+                        ` : ''}
                         
                         ${this.isShowProfileContainer ? html`
-                        <div class="profile-container">
-                            <p>profile-container</p>
-                        </div>` : html``}
+                            <profile-container></profile-container>
+                        ` : html``}
                         
                     </div>
                 </div>
@@ -2954,6 +3043,7 @@ class DashBoard extends LitElement {
     this.getUserInfo();
     this.shopList = [];
     this.userFullName = 'Ім. Пр.';
+    this.isShowShopListContainer = true;
   }
 
   _buildUrlForShop(item) {
