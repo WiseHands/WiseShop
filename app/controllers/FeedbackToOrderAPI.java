24b45e95edfaccb54cf515b082238e92c25e02c6
Alexander <tatrun89@gmail.com>
2020-05-25 15:58:09 +0300

    Created feedback to order json object and send it to API

--- app/controllers/FeedbackToOrderAPI.java
+++ app/controllers/FeedbackToOrderAPI.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class FeedbackToOrderAPI extends AuthController{
 
-    public static void createFeedbackFromClient(){
+    public static void createFeedbackFromClient() throws Exception{
+        JSONParser parser = new JSONParser();
+        JSONObject jsonBody = (JSONObject) parser.parse(params.get("body"));
+        System.out.println("createFeedbackFromClient jsonBody: " + jsonBody);
 
     }
 
