7c1a440906f759e6b3e94f68c6ab7298f2a768c0
Alexander <tatrun89@gmail.com>
2020-06-04 16:23:51 +0300

    wip reviews, prepared controller for page

--- wisehands/admin/js/controllers/ProductReviewsController.js
+++ wisehands/admin/js/controllers/ProductReviewsController.js
@@ -4,11 +4,10 @@ angular.module('WiseHands')
             $scope.loading = true;
             $http({
                 method: 'GET',
-                url: '/addition/get-all/' + $routeParams.uuid
+                url: '/api/feedback/get/list/' + $routeParams.uuid
             })
                 .then(function successCallback(response) {
-                    $scope.properties = response.data;
-                    console.log("/addition/get-all/" , response.data);
+                    console.log("/api/feedback/get/list/" , response.data);
                 }, function errorCallback(error) {
                     $scope.loading = false;
                     console.log(error);
@@ -20,46 +19,12 @@ angular.module('WiseHands')
             })
                 .then(function successCallback(response) {
                     $scope.loading = false;
-                    $scope.product = response.data;
-                    $scope.activeShop = localStorage.getItem('activeShop');
-                    $scope.product.images.forEach(function(image, index){
-                        if(image.uuid === $scope.product.mainImage.uuid){
-                            $scope.selected = index;
-                        }
-                    })
-
+                    console.log("/api/feedback/get/list/" , response.data);
                 }, function errorCallback(error) {
                     $scope.loading = false;
                     console.log(error);
                 });
 
-
-            $scope.select= function(index) {
-            $scope.selected = index;
-            };
-            $scope.hideModal = function () {
-                $('#deleteProduct').modal('hide');
-                $('body').removeClass('modal-open');
-                $('.modal-backdrop').remove();
-            };
-            $scope.deleteButton = true;
-            $scope.deleteProduct = function () {
-                $scope.deleteButton = false;
-                $scope.modalSpinner = true;
-                $http({
-                    method: 'DELETE',
-                    url: '/api/product/' + $routeParams.uuid,
-                })
-                    .then(function successCallback(response) {
-                        $scope.modalSpinner = false;
-                        $scope.succesfullDelete = true;
-
-                    }, function errorCallback(response) {
-                        $scope.modalSpinner = false;
-                        console.log(response);
-                    });
-
-            };
             $scope.goBack = function () {
                 window.history.back();
             }
