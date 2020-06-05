95f5658d73f8e0643af786a0167c0e95f733645b
toluene5019 <42148663+toluene5019@users.noreply.github.com>
2020-06-05 15:55:54 +0300

    wip  reviews page

--- wisehands/admin/js/controllers/ProductReviewsController.js
+++ wisehands/admin/js/controllers/ProductReviewsController.js
@@ -2,23 +2,22 @@ angular.module('WiseHands')
     .controller('ProductReviewsController', ['$http', '$scope', '$routeParams', 'signout', function($http, $scope, $routeParams, signout) {
             $scope.uuid = $routeParams.uuid;
             $scope.loading = true;
-            $http({
-                method: 'GET',
-                url: '/api/feedback/get/list/' + $routeParams.uuid
-            })
-                .then(function successCallback(response) {
-                    console.log("/api/feedback/get/list/" , response.data);
-                }, function errorCallback(error) {
-                    $scope.loading = false;
-                    console.log(error);
-                });
 
             $http({
                 method: 'GET',
                 url: '/api/product/' + $routeParams.uuid
             })
                 .then(function successCallback(response) {
+                    $scope.product = response.data;
                     $scope.loading = false;
+
+                    $scope.activeShop = localStorage.getItem('activeShop');
+                    $scope.product.images.forEach(function(image, index){
+                        if(image.uuid === $scope.product.mainImage.uuid){
+                            $scope.selected = index;
+                        }
+                    })
+
                     console.log("/api/feedback/get/list/" , response.data);
                 }, function errorCallback(error) {
                     $scope.loading = false;
