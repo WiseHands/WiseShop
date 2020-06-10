86b8a9c7e0f69cacc7b0be313741ca484436ef95
Reverie <patlavovach@gmail.com>
2020-06-11 02:26:22 +0300

    show/hide review functionality

--- wisehands/admin/js/controllers/ProductReviewsController.js
+++ wisehands/admin/js/controllers/ProductReviewsController.js
@@ -1,60 +1,40 @@
 angular.module('WiseHands')
-    .controller('ProductReviewsController', ['$http', '$scope', '$routeParams', 'signout', function($http, $scope, $routeParams, signout) {
-            $scope.uuid = $routeParams.uuid;
-            $scope.loading = true;
+  .controller('ProductReviewsController', ['$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+    $scope.loading = true;
 
-        $http({
-                method: 'GET',
-                url: '/api/product/' + $routeParams.uuid
-            })
-                .then(function successCallback(response) {
-                    const product = response.data;
-                    $scope.loading = false;
-                    parseProductData(product);
+    $http({
+      method: 'GET',
+      url: `/api/product/${$routeParams.uuid}`
+    })
+      .then(response => {
+        const product = response.data;
+        parseProductData(product);
+        $scope.activeShop = localStorage.getItem('activeShop');
+        $scope.selected = $scope.product.images.findIndex(item => item.uuid === $scope.product.mainImage.uuid);
+        $scope.loading = false;
+      }, error => {
+        $scope.loading = false;
+        console.log(error);
+      });
 
-                    $scope.activeShop = localStorage.getItem('activeShop');
-                    $scope.product.images.forEach(function(image, index){
-                        if(image.uuid === $scope.product.mainImage.uuid){
-                            $scope.selected = index;
-                        }
-                    })
+    function parseProductData(product) {
+      product.feedbackList.map(item => {
+        item.feedbackTime = moment(item.feedbackTime).format('DD MMMM YYYY HH:mm:ss');
+        return item;
+      });
+      $scope.product = product;
+    }
 
-                    console.log("get/list/" , response.data);
-                }, function errorCallback(error) {
-                    $scope.loading = false;
-                    console.log(error);
-                });
+    $scope.showOrHideFeedback = event => {
+      const review = event.review;
+      const url = event.review.showReview ? `/api/feedback/hide/${review.uuid}` : `/api/feedback/show/${review.uuid}`;
+      sendParamsToFeedbackAPI(url, review);
+    };
 
-            function parseProductData(product) {
-                product.feedbackList.map(item => {
-                    item.feedbackTime = moment(item.feedbackTime).format('DD MMMM YYYY HH:mm:ss');
-                    return item;
-                })
-                $scope.product = product;
-            }
-
-            $scope.showFeedback = (event) => {
-               const url = `/api/feedback/show/${event.review.uuid}`;
-               sendParamsToFeedbackAPI(url);
-            }
-
-            $scope.hideFeedback = (event) => {
-               const url = `/api/feedback/hide/${event.review.uuid}`;
-               sendParamsToFeedbackAPI(url);
-            }
-
-            function sendParamsToFeedbackAPI(url){
-
-            $http({
-              method: 'PUT',
-              url: url
-            }).then((data) => {
-               console.log("sendParamsShowFeedback " , data);
-            })
-
-            }
-
-            $scope.goBack = function () {
-                window.history.back();
-            }
-    }]);
+    function sendParamsToFeedbackAPI(url, review) {
+      $http({
+        method: 'PUT',
+        url: url
+      }).then(() => review.showReview = !review.showReview)
+    }
+  }]);
