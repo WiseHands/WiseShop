4bdd7f9407a0f2e9950df05a0b00fa358ac33633
Reverie <patlavovach@gmail.com>
2020-06-26 03:05:56 +0300

    validation for courier delivery settings

--- wisehands/admin/js/controllers/DeliverySettingsController.js
+++ wisehands/admin/js/controllers/DeliverySettingsController.js
@@ -1,54 +1,68 @@
 angular.module('WiseHands')
-    .controller('DeliverySettingsController', ['$scope', '$http', '$location', 'sideNavInit', 'signout', function ($scope, $http, $location, sideNavInit, signout) {
-        $scope.loading = true;
-        
-        $http({
-            method: 'GET',
-            url: '/delivery',
+  .controller('DeliverySettingsController', ['$scope', '$http', '$location', 'sideNavInit', 'signout', function ($scope, $http, $location, sideNavInit, signout) {
+    $scope.loading = true;
 
-        })
-            .then(function successCallback(response) {
-                $scope.loading = false;
-                $scope.delivery = response.data;
-            }, function errorCallback(response) {
-                $scope.loading = false;
-            });
-        $scope.setDeliveryOptions = function () {
-            $scope.loading = true;
-            $http({
-                method: 'PUT',
-                url: '/delivery',
-                data: $scope.delivery,
+    $http({
+      method: 'GET',
+      url: '/delivery',
 
-            })
-                .then(function successCallback(response) {
-                    $scope.loading = false;
-                    $location.path('/delivery');
-                    showInfoMsg("SAVED");
-                }, function errorCallback(response) {
-                    $scope.loading = false;
-                    console.log(response);
-                    showWarningMsg("ERROR");
-                });
+    })
+      .then(function successCallback(response) {
+        $scope.loading = false;
+        $scope.delivery = response.data;
+      }, function errorCallback(response) {
+        $scope.loading = false;
+      });
 
-        };
-        
-        sideNavInit.sideNav();
-       
-    }]);
+    $scope.setDeliveryOptions = function () {
+      if (!validate()) return;
+      $scope.loading = true;
+      $http({
+        method: 'PUT',
+        url: '/delivery',
+        data: $scope.delivery,
+
+      })
+        .then(function successCallback(response) {
+          $scope.loading = false;
+          $location.path('/delivery');
+          showInfoMsg("SAVED");
+        }, function errorCallback(response) {
+          $scope.loading = false;
+          console.log(response);
+          showWarningMsg("ERROR");
+        });
+
+    };
+
+    function validate() {
+      const isCourierPriceMoreThanOrEqualZero = $scope.delivery.courierPrice >= 0;
+      const isFreeDeliveryPriceMoreThanOrEqualZero = $scope.delivery.courierFreeDeliveryLimit >= 0;
+      const isValid = isCourierPriceMoreThanOrEqualZero && isFreeDeliveryPriceMoreThanOrEqualZero;
+      $scope.showCouriePriceValidationError = !isCourierPriceMoreThanOrEqualZero;
+      $scope.showFreDeliveryPricePriceValidationError = !isFreeDeliveryPriceMoreThanOrEqualZero;
+      return isValid;
+
+    }
+
+    sideNavInit.sideNav();
+
+  }]);
+
 function showWarningMsg(msg) {
-    toastr.clear();
-    toastr.options = {
-        "positionClass": "toast-bottom-right",
-        "preventDuplicates": true
-    };
-    toastr.warning(msg);
+  toastr.clear();
+  toastr.options = {
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": true
+  };
+  toastr.warning(msg);
 }
+
 function showInfoMsg(msg) {
-    toastr.clear();
-    toastr.options = {
-        "positionClass": "toast-bottom-right",
-        "preventDuplicates": true
-    };
-    toastr.info(msg);
+  toastr.clear();
+  toastr.options = {
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": true
+  };
+  toastr.info(msg);
 }
