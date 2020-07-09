425cfac9d21388d6bdbbbc0d1469925c7c0d7e9e
Aleksander <tatrun89@gmail.com>
2020-07-09 20:30:29 +0300

    wip translation

--- app/services/translaiton/Translation.java
+++ app/services/translaiton/Translation.java
@@ -8,22 +8,26 @@ import java.util.List;
 public class Translation {
 
     public static ProductDTO setTranslationForProduct(String language, ProductDTO product){
-        List<TranslationItemDTO> translationNameList = product.productNameTextTranslationBucket.translationList;
-        for(TranslationItemDTO item : translationNameList){
-            if (item.language == null){
-                item.language = language;
-            }
-            if (item.language.equals(language)){
-                product.name = item.content;
+        if(product.productNameTextTranslationBucket != null){
+            List<TranslationItemDTO> translationNameList = product.productNameTextTranslationBucket.translationList;
+            for(TranslationItemDTO item : translationNameList){
+                if (item.language == null){
+                    item.language = language;
+                }
+                if (item.language.equals(language)){
+                    product.name = item.content;
+                }
             }
         }
-        List<TranslationItemDTO> translationDescriptionList = product.productDescriptionTextTranslationBucket.translationList;
-        for(TranslationItemDTO item : translationDescriptionList){
-            if (item.language == null){
-                item.language = language;
-            }
-            if (item.language.equals(language)){
-                product.description = item.content;
+        if(product.productDescriptionTextTranslationBucket !=null){
+            List<TranslationItemDTO> translationDescriptionList = product.productDescriptionTextTranslationBucket.translationList;
+            for(TranslationItemDTO item : translationDescriptionList){
+                if (item.language == null){
+                    item.language = language;
+                }
+                if (item.language.equals(language)){
+                    product.description = item.content;
+                }
             }
         }
         return product;
