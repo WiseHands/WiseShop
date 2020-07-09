275ded0cd4cc76dae587ac874b1b028aa8e27e02
Aleksander <tatrun89@gmail.com>
2020-07-09 21:03:34 +0300

    fix translation

--- wisehands/admin/js/controllers/ProductTranslationController.js
+++ wisehands/admin/js/controllers/ProductTranslationController.js
@@ -26,26 +26,38 @@ angular.module('WiseHands')
             setContentForProductLabel = (product) => {
                 let productNameTextTranslationBucket = '';
                 let englishNameLabel = '';
+                let ukrainianNameLabel = '';
                 if(product.productNameTextTranslationBucket){
                     productNameTextTranslationBucket = product.productNameTextTranslationBucket.uuid;
+                    ukrainianNameLabel = product.productNameTextTranslationBucket.translationList[0].content;
                     englishNameLabel = product.productNameTextTranslationBucket.translationList[1].content;
                 }
 
                 let isTranslationForProductName = $scope.translationUuid === productNameTextTranslationBucket;
                 if (isTranslationForProductName){
-                    textInUkrainian.value = product.name;
+                    if(ukrainianNameLabel === ""){
+                        textInUkrainian.value = product.name;
+                    } else {
+                        textInUkrainian.value = ukrainianNameLabel;
+                    }
                     textInEnglish.value = englishNameLabel;
                 }
 
                 let productDescriptionTextTranslationBucket = '';
                 let englishDescriptionLabel = '';
+                let ukrainianDescriptionLabel = '';
                 if (product.productDescriptionTextTranslationBucket){
                     productDescriptionTextTranslationBucket = product.productDescriptionTextTranslationBucket.uuid;
+                    ukrainianDescriptionLabel = product.productDescriptionTextTranslationBucket.translationList[0].content;
                     englishDescriptionLabel = product.productDescriptionTextTranslationBucket.translationList[1].content;
                 }
                 let  isTranslationForDescription = $scope.translationUuid === productDescriptionTextTranslationBucket;
                 if (isTranslationForDescription){
-                    textInUkrainian.value = product.description;
+                    if(ukrainianDescriptionLabel === ""){
+                        textInUkrainian.value = product.description;
+                    } else {
+                        textInUkrainian.value = ukrainianDescriptionLabel;
+                    }
                     textInEnglish.value = englishDescriptionLabel;
                 }
             };
