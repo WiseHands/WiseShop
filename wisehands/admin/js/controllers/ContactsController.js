3b10384b022bb9cf81e3d88360a7641bb91f7614
Alexander <tatrun89@gmail.com>
2020-08-14 13:15:33 +0300

    added preview map to contact page

--- wisehands/admin/js/controllers/ContactsController.js
+++ wisehands/admin/js/controllers/ContactsController.js
@@ -4,11 +4,23 @@ angular.module('WiseHands')
 
         $http({
             method: 'GET',
+            url: '/shop/details',
+        })
+            .then((response) => {
+                $scope.activeShop = response.data;
+                console.log("$scope.activeShop", $scope.activeShop);
+                $scope.loading = false;
+            }, errorCallback = (response) => {
+            });
+
+        $http({
+            method: 'GET',
             url: '/contact/details'
         })
             .then((response) => {
                 $scope.loading = false;
                 $scope.contacts = response.data;
+                $scope.shopLocation = response.data.shopLocation;
                 console.log("$scope.contacts", $scope.contacts);
             }, (errorCallback) => {
                 $scope.loading = false;
