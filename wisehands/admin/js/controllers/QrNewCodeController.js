68c55b0cc8960487285ae1b11d1eda1c854ec99c
Alexander <tatrun89@gmail.com>
2020-09-08 23:03:49 +0300

    added qr component and generator for qr-code

--- wisehands/admin/js/controllers/QrNewCodeController.js
+++ wisehands/admin/js/controllers/QrNewCodeController.js
@@ -2,7 +2,32 @@ angular.module('WiseHands')
     .controller('QrNewCodeController', ['$scope', '$http', 'signout', 'sideNavInit', 'shared',
         function ($scope, $http, signout, sideNavInit, shared) {
 
+        function generateQRCode() {
+            var qrtext = document.getElementById("qr-text").value;
 
+            alert(qrtext);
+            qr.set({
+                foreground: 'black',
+                size: 200,
+                value: qrtext
+            });
+        }
+
+        $scope.createQrCode = () => {
+             let qrtext = document.querySelector('#table_name').value;
+
+             let qr = new QRious({
+                 element: document.getElementById('qr-code'),
+                 size: 200,
+                 value: qrtext
+             });
+
+//             qr.set({
+//                 foreground: 'black',
+//                 size: 200,
+//                 value: qrtext
+//             });
+        }
 
         sideNavInit.sideNav();
     }]);
