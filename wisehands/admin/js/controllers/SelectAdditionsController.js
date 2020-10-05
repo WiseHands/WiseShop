e63de4a1a0b7389049e557bd3fd2fe2db3bdf412
Alexander <tatrun89@gmail.com>
2020-10-05 12:22:04 +0300

    wip additions

--- wisehands/admin/js/controllers/SelectAdditionsController.js
+++ wisehands/admin/js/controllers/SelectAdditionsController.js
@@ -2,64 +2,30 @@ angular.module('WiseHands')
     .controller('SelectAdditionsController', ['$scope', '$http', 'signout', 'sideNavInit', 'shared', '$routeParams', '$location', '$window',
         function ($scope, $http, signout, sideNavInit, shared, $routeParams, $location, $window) {
 
-        console.log('location => ', $location);
-
+        console.log('$routeParams => ', $routeParams.productUuid);
         $http({
             method: 'GET',
-            url: `/api/qr/info/${$routeParams.uuid}`
+            url: `/api/product/${$routeParams.productUuid}`
         }).then(response => {
-            console.log(response);
-            $scope.qr = response.data;
-            showQR($scope.qr);
+            console.log('product', response);
+            $scope.product = response.data;
+            $scope.productAdditions = response.data.additions;
+
         }, error => {
             console.log(error);
         });
 
-        $scope.redirectToEdit = () => {
-            $window.location.href = `#/qredit/${$routeParams.productUuid}`;
-        }
-
-        showQR = (qrCode) =>{
-           let url = _generateUrlForQr(qrCode.uuid);
-           new QRious({
-             element: document.getElementById('qr-code'),
-             size: 200,
-             value: url
-           });
-        }
-
-        _generateUrlForQr = (uuid) => {
-            let domain, hostname = $window.location.hostname;
-            if (hostname === 'localhost'){
-                domain = hostname + ':3334'
-            } else {
-                domain = hostname;
-            }
-           return 'https://' + domain + '/?qr_uuid='+ uuid;
-        }
-
-        $scope.deleteButton = true;
-
-        $scope.removeQr = () => {
-            $http({
-                method: "DELETE",
-                url: `/api/qr/delete/${$scope.qr.uuid}`,
-            }).then(response => {
-                $('#removeQR').modal('hide');
-                if(response.status === 200){
-                     $location.path('/qrcontroller');
-                }
-            }, error => {
+        $http({
+            method: 'GET',
+            url: '/api/addition/list'
+        })
+            .then(function successCallback(response) {
+                $scope.availableAdditions = response.data;
+                console.log("/addition/get-all/" , $scope.availableAdditions);
+            }, function errorCallback(error) {
+                $scope.loading = false;
                 console.log(error);
             });
-        }
-
-        $scope.downloadQrCode = () => {
-            let a = document.createElement("a"); //Create <a>
-            a.href = document.querySelector('#qr-code').src; //Image Base64 Goes here
-            a.download = `${$scope.qr.name}.png`; //File name Here
-            a.click();
-        }
 
         sideNavInit.sideNav();
     }]);
