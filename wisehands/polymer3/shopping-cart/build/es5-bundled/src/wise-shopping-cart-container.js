9ca02ca9bd4be981f8ec8eabdd00ace119222252
TarasH1 <research010@gmail.com>
2020-10-09 17:04:27 +0300

    shopping cart bug fixes

--- wisehands/polymer3/shopping-cart/build/es5-bundled/src/wise-shopping-cart-container.js
+++ wisehands/polymer3/shopping-cart/build/es5-bundled/src/wise-shopping-cart-container.js
@@ -25690,7 +25690,7 @@ class WiseShoppingCartContainer extends PolymerElement {
 
     if (isValid && !isCourierDeliverySelected) {
       const postDepartmentNumber = this.shadowRoot.getElementById('clientPostDepartmentNumber');
-      console.log("postDepartmentNumber => ", postDepartmentNumber.value);
+      //console.log("postDepartmentNumber => ", postDepartmentNumber.value);
       this.cart.client.postDepartamentInfo.postDepartmentNumber = postDepartmentNumber.value;
 
       const cart = {
