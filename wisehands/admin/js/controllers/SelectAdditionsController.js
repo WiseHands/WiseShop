3a699ef5b09fdb15d355ee273541b718a79cb7c8
Reverie <patlavovach@gmail.com>
2020-10-26 22:42:17 +0200

    select additions controller - correct logic behavior for selected/default additions

--- wisehands/admin/js/controllers/SelectAdditionsController.js
+++ wisehands/admin/js/controllers/SelectAdditionsController.js
@@ -1,70 +1,44 @@
 angular.module('WiseHands')
-    .controller('SelectAdditionsController', ['$scope', '$http', 'signout', 'sideNavInit', 'shared', '$routeParams', '$location', '$window',
-        function ($scope, $http, signout, sideNavInit, shared, $routeParams, $location, $window) {
+  .controller('SelectAdditionsController', ['$scope', '$http', 'sideNavInit', '$routeParams',
+    function ($scope, $http, sideNavInit, $routeParams) {
 
-        console.log('$routeParams => ', $routeParams.productUuid);
-        let isAdditionDefault = document.querySelector('#additionDefault');
+      $http({
+        method: 'GET',
+        url: `/api/product/${$routeParams.productUuid}`
+      }).then(response => {
+          $scope.activeShop = localStorage.getItem('activeShop');
+          $scope.product = response.data;
+        }, error => console.log(error)
+      );
+
+      $http({
+        method: 'GET',
+        url: '/api/addition/list'
+      })
+        .then(response => $scope.availableAdditions = response.data,
+          error => console.log(error)
+        );
+
+      $scope.selectedDefaultAddition = (event, {addition}) => {
+        event.stopPropagation();
+        addition.isSelected = addition.isDefault;
+      };
+
+      $scope.selectedAddition = ({addition}) => {
+        addition.isSelected = !addition.isSelected;
+        if (addition.isDefault) addition.isDefault = !addition.isDefault;
+        const url = addition.isSelected ? `/api/addition/add/${$routeParams.productUuid}/${addition.uuid}/${addition.isDefault}` : `/api/addition/remove/${$routeParams.productUuid}/${addition.uuid}`;
 
         $http({
-            method: 'GET',
-            url: `/api/product/${$routeParams.productUuid}`
-        }).then(response => {
-            console.log('product', response);
-            $scope.activeShop = localStorage.getItem('activeShop');
-            $scope.product = response.data;
-            $scope.productAdditions = response.data.additions;
+          method: 'PUT',
+          url: url
+        }).then(response => addition.isSelected = response.data.isSelected,
+          error => console.log(error)
+        )
+        ;
+      };
 
-        }, error => {
-            console.log(error);
-        });
-
-        $http({
-            method: 'GET',
-            url: '/api/addition/list'
-        })
-            .then(function successCallback(response) {
-                $scope.availableAdditions = response.data;
-                console.log("/addition/get-all/" , $scope.availableAdditions);
-                showSelectedAddition($scope.availableAdditions);
-            }, function errorCallback(error) {
-                $scope.loading = false;
-                console.log(error);
-            });
-
-        showSelectedAddition = () => {
-            console.log('show selected addition => ', $scope.availableAdditions, $scope.productAdditions);
-            $scope.productAdditions.forEach( _productAddition => {
-                checkAddition(_productAddition);
-            })
-        }
-
-        checkAddition = (_productAddition) => {
-           console.log('_availableAddition => ', _productAddition);
-        }
-
-        $scope.selectedDefaultAddition = (event) => {
-             console.log("selectedDefaultAddition (checkbox) =>" , event);
-        }
-
-        $scope.selectedAddition = (event) => {
-            console.log("selectedAddition =>" , event.$index);
-            const addition = event.addition;
-            addition.isSelected ? addition.isSelected = false : addition.isSelected = true;
-            const url = addition.isSelected ? `/api/addition/add/${$routeParams.productUuid}/${addition.uuid}/${addition.isDefault}` : `/api/addition/remove/${$routeParams.productUuid}/${addition.uuid}`;
-
-            $http({
-                method: 'PUT',
-                url: url
-            }).then(response => {
-                console.log('product addition/add/remove', response);
-                addition.isSelected = response.data.isSelected;
-                //addition.isSelected ? $scope.productAdditions.push(response.data) : $scope.productAdditions.length === 1 ? $scope.productAdditions = [] : $scope.productAdditions.splice(event.$index, 1);
-            }, error => {
-                console.log(error);
-            });
-        }
-
-        sideNavInit.sideNav();
+      sideNavInit.sideNav();
     }]);
 
 
