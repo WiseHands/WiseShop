14d3f1f3920508abf017e170567b0b9977449d85
Aleksander <tatrun89@gmail.com>
2020-11-01 00:46:01 +0200

    refactor select additions controller

--- wisehands/admin/js/controllers/SelectAdditionsController.js
+++ wisehands/admin/js/controllers/SelectAdditionsController.js
@@ -1,70 +1,58 @@
 angular.module('WiseHands')
-  .controller('SelectAdditionsController', ['$scope', '$http', 'sideNavInit', '$routeParams',
-    function ($scope, $http, sideNavInit, $routeParams) {
-
-      const selectedAdditions = [];
-
-      $http({
-        url: `/api/product/${$routeParams.productUuid}`
-      }).then(({data}) => {
-          $scope.activeShop = localStorage.getItem('activeShop');
-          $scope.product = data;
-          _getAvailableAdditions(data.selectedAddition);
-        }, error => console.log(error)
-      );
-
-      const _getAvailableAdditions = selectedAddition => {
-        $http({
-          url: '/api/addition/list'
-        }).then(({data}) => _setAvailableAdditions(data, selectedAddition),
-          error => console.log(error)
-        );
-      };
-
-      const _setAvailableAdditions = (availableAdditions, selectedAddition) => {
-        const parsedAdditions = [];
-        availableAdditions.forEach(availableAddition => {
-          const match = selectedAddition.find(item => item.addition.uuid === availableAddition.uuid);
-          parsedAdditions.push({...availableAddition, ...match});
-        });
-        $scope.availableAdditions = parsedAdditions;
-      };
-
-      $scope.selectDefaultAddition = (event, {addition}) => {
-        event.stopPropagation();
-        addition.isSelected = addition.isDefault;
-        addition.productUuid = $routeParams.productUuid;
-        if (selectedAdditions.length === 0) {
-            selectedAdditions.push(addition);
-        }
-        console.log("selectedDefaultAddition", addition);
-      };
-
-      $scope.selectAddition = ({addition}) => {
-        addition.isSelected = !addition.isSelected;
-        addition.productUuid = $routeParams.productUuid;
-        selectedAdditions.push(addition);
-        console.log("selectAddition", addition);
-
-      };
-
-      const sendSelectedAddition = (url, selectedAdditions) => {
-        $http({
-          method: 'PUT',
-          url: url,
-          data: selectedAdditions
-        }).then(({data}) => console.log(data),
-            error => console.log(error)
-        );
-      };
-
-      $scope.saveSelectedAdditions = () => {
-        console.log('saveSelectedAdditions => ', JSON.stringify(selectedAdditions));
-        const url = `/api/addition/save/all`;
-        sendSelectedAddition(url, JSON.stringify(selectedAdditions));
-      };
-
-      sideNavInit.sideNav();
-    }]);
+	.controller('SelectAdditionsController', ['$scope', '$http', 'sideNavInit', '$routeParams',
+		function ($scope, $http, sideNavInit, $routeParams) {
+			const productUuid = $routeParams.productUuid;
+			
+			$http({
+				url: `/api/product/${productUuid}`
+			}).then(({data}) => {
+					$scope.activeShop = localStorage.getItem('activeShop');
+					$scope.product = data;
+					_getAvailableAdditions(data.selectedAddition);
+				}, error => console.log(error)
+			);
+			
+			const _getAvailableAdditions = additions => {
+				$http({
+					url: '/api/addition/list'
+				}).then(({data}) => _setAvailableAdditions(data, additions),
+					error => console.log(error)
+				);
+			};
+			
+			const _setAvailableAdditions = (availableAdditions, additions) => {
+				const parsedAdditions = [];
+				availableAdditions.forEach(availableAddition => {
+					const match = additions.find(item => item.addition.uuid === availableAddition.uuid);
+					parsedAdditions.push({...availableAddition, ...match});
+				});
+				$scope.availableAdditions = parsedAdditions;
+			};
+			
+			$scope.selectDefaultAddition = (event, {addition}) => {
+				event.stopPropagation();
+				addition.isSelected = addition.isDefault;
+			};
+			
+			$scope.selectAddition = ({addition}) => {
+				addition.isSelected = !addition.isSelected;
+			};
+			
+			$scope.saveAdditions = () => {
+				const parsedAdditions = $scope.availableAdditions.map(item => {
+					item.productUuid = productUuid;
+					return item;
+				});
+				$http({
+					method: 'PUT',
+					url: '/api/addition/save/all',
+					data: parsedAdditions
+				}).then({},
+					error => console.log(error)
+				);
+			};
+			
+			sideNavInit.sideNav();
+		}]);
 
 
