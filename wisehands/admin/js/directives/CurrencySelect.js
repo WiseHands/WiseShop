350cbcae44b802dcc565dc8a05987a42a7439a63
Reverie <patlavovach@gmail.com>
2021-01-18 02:26:02 +0200

    currency select dropdown prototype

--- wisehands/admin/js/directives/CurrencySelect.js
+++ wisehands/admin/js/directives/CurrencySelect.js
@@ -1,51 +1,22 @@
 angular.module('WiseHands')
-    .directive('ngCurrencySelect', ['LocaleService', function (LocaleService) { 'use strict';
-
-        return {
-            restrict: 'A',
-            replace: true,
-            template: ''+
-            '<div class="language-select" ng-if="visible">'+
-            '<p>'+
-            '{{"directives.currency-select.Currency" | translate}}:'+
-            '<select class="form-control" ng-model="currentLocaleDisplayName"'+
-            'ng-options="localesDisplayName for localesDisplayName in localesDisplayNames"'+
-            'ng-change="changeLanguage(currentLocaleDisplayName); changeLocale(currentLocaleDisplayName)">'+
-            '</select>'+
-            '</p>'+
-            '</div>'+
-            '',
-            controller: ['$scope','$http', function ($scope, $http) {
-                $scope.currentLocaleDisplayName = LocaleService.getLocaleDisplayName();
-                $scope.localesDisplayNames = LocaleService.getLocalesDisplayNames();
-                $scope.visible = $scope.localesDisplayNames &&
-                    $scope.localesDisplayNames.length > 1;
-
-                $scope.changeLanguage = function (locale) {
-                    LocaleService.setLocaleByDisplayName(locale);
-
-                };
-                $scope.changeLocale = function (language) {
-                    var currentLocale = '';
-                    if (language === 'Українська') {
-                        currentLocale = 'uk_UA'
-                    } else if (language === 'English') {
-                        currentLocale = 'en_US'
-                    } else if (language === 'Polski') {
-						currentLocale = 'pl_PL'
-					}
-                    $http({
-                        method: 'PUT',
-                        url: '/shop/' + currentLocale
-                    })
-                        .success(function (response) {
-                            localStorage.setItem('locale', currentLocale);
-                        }).
-                    error(function (response) {
-                        $scope.loading = false;
-                        console.log(response);
-                    });
-                };
-            }]
-        };
-    }]);
+  .controller(['$scope', function($scope) {
+    $scope.customer = {
+      name: 'Naomi',
+      address: '1600 Amphitheatre'
+    };
+  }])
+  .directive('myCustomer', function() {
+    return {
+      template: ''+
+      '<div class="language-select" ng-if="visible">'+
+      '<p>'+
+      '{{"directives.currency-select.Currency" | translate}}:'+
+      '<select class="form-control" ng-model="currentLocaleDisplayName"'+
+      'ng-options="localesDisplayName for localesDisplayName in localesDisplayNames"'+
+      'ng-change="changeLanguage(currentLocaleDisplayName); changeLocale(currentLocaleDisplayName)">'+
+      '</select>'+
+      '</p>'+
+      '</div>'+
+      ''
+    };
+  });
