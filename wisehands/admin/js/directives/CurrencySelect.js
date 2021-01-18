c7a27b2b30e821858f2c3599aa49411f9b61a860
Aleksander <tatrun89@gmail.com>
2021-01-18 16:18:26 +0200

    added currency selected in shop setting page

--- wisehands/admin/js/directives/CurrencySelect.js
+++ wisehands/admin/js/directives/CurrencySelect.js
@@ -1,22 +1,46 @@
 angular.module('WiseHands')
-  .controller(['$scope', function($scope) {
-    $scope.customer = {
-      name: 'Naomi',
-      address: '1600 Amphitheatre'
-    };
-  }])
-  .directive('myCustomer', function() {
+  .directive('ngCurrencySelect', function() {
     return {
       template: ''+
       '<div class="language-select" ng-if="visible">'+
       '<p>'+
       '{{"directives.currency-select.Currency" | translate}}:'+
-      '<select class="form-control" ng-model="currentLocaleDisplayName"'+
-      'ng-options="localesDisplayName for localesDisplayName in localesDisplayNames"'+
-      'ng-change="changeLanguage(currentLocaleDisplayName); changeLocale(currentLocaleDisplayName)">'+
+      '<select class="form-control" ng-model="shopCurrency"'+
+      'ng-options="localesDisplayName for localesDisplayName in showAvailableCurrencies"'+
+      'ng-change="changeCurrency(shopCurrency)">'+
       '</select>'+
       '</p>'+
       '</div>'+
-      ''
+      '',
+	    controller: ['$scope','$http', function ($scope, $http) {
+		    $scope.showAvailableCurrencies = ['UAH', 'USD', 'EUR'];
+		    $scope.visible = $scope.showAvailableCurrencies &&
+			    $scope.showAvailableCurrencies.length > 1;
+
+		    $http({
+			    method: 'GET',
+			    url: '/shop/details',
+		    })
+			    .then(response => {
+				    console.log("response in currency drop-btn => ", response.data.currencyShop.currency);
+				    $scope.shopCurrency = response.data.currencyShop.currency;
+			    }, error => {
+				    console.log(error);
+			    });
+		    
+		    $scope.changeCurrency = function (currency) {
+			    var currentLocale = '';
+			    console.log("changeCurrency => ", currency);
+			    // $http({
+				   //  method: 'PUT',
+				   //  url: '/shop/' + currentLocale
+			    // })
+				   //  .success(function (response) {
+				   //  }).
+			    // error(function (response) {
+				   //  console.log(response);
+			    // });
+		    };
+	    }]
     };
   });
