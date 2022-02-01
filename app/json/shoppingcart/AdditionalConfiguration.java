a69b15b88d90e27128e46a2d4c2e53dee49f5f16
Reverie <patlavovach@gmail.com>
2022-02-01 20:49:18 +0200

    additional payment feature - add additional payment to additional configuration for shopping cart

--- app/json/shoppingcart/AdditionalConfiguration.java
+++ app/json/shoppingcart/AdditionalConfiguration.java
@@ -2,6 +2,7 @@ package json.shoppingcart;
 
 import com.google.gson.annotations.Expose;
 import models.BannerDTO;
+import models.PaymentSettingsDTO;
 
 import javax.persistence.Transient;
 
@@ -14,8 +15,13 @@ public class AdditionalConfiguration {
     @Transient
     public BannerDTO banner;
 
-    public AdditionalConfiguration(String labelForCustomerName, BannerDTO banner) {
+    @Expose
+    @Transient
+    public PaymentSettingsDTO additionalPayment;
+
+    public AdditionalConfiguration(String labelForCustomerName, BannerDTO banner, PaymentSettingsDTO additionalPayment) {
         this.labelForCustomerName = labelForCustomerName;
         this.banner = banner;
+        this.additionalPayment = additionalPayment;
     }
 }
