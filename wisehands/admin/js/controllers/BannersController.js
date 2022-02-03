6166e407e56f81c8013f1ec0c9655a79dbf84e49
Reverie <patlavovach@gmail.com>
2022-02-03 11:45:28 +0200

    tweaked FE logic for banners a little bit

--- wisehands/admin/js/controllers/BannersController.js
+++ wisehands/admin/js/controllers/BannersController.js
@@ -1,63 +1,54 @@
-
 angular.module('WiseHands')
-  .controller('BannersController', ['$scope', '$http', 'signout', 'sideNavInit', '$window', function ($scope, $http, signout, sideNavInit, $window) {
-    $scope.loading = true;
-    const activeShop = localStorage.getItem('activeShop');
+    .controller('BannersController', ['$scope', '$http', 'signout', 'sideNavInit', function ($scope, $http, signout, sideNavInit) {
+        $scope.loading = true;
 
-    $http({
-      method: 'GET',
-      url: '/api/banners',
-    })
-      .then(response => {
-        $scope.loading = false;
-        $scope.bannerProductOfDay = response.data[0]
-        console.log('/api/banners response', $scope.bannerProductOfDay)
-        $scope.bannerForShopBasket = response.data[1]
-        console.log('/api/banners response', $scope.bannerForShopBasket)
-        console.log('/api/banners response', response.data)
-
-      }, error => {
-        $scope.loading = false;
-        console.log(error);
-      });
+        $http({
+            method: 'GET',
+            url: '/api/banners',
+        })
+            .then(({data}) => {
+                $scope.loading = false;
+                $scope.bannerProductOfDay = data.find?.(item => item.type === 'DISH_OF_DAY') || {};
+                $scope.bannerForShopBasket = data.find?.(item => item.type === 'BASKET') || {};
+            }, error => {
+                $scope.loading = false;
+                console.log(error);
+            });
 
 
+        $scope.setBannerForProductOfDay = () => {
+            $scope.loading = true;
+            $http({
+                method: 'PUT',
+                url: '/api/banner/for/product',
+                data: $scope.bannerProductOfDay
+            })
+                .success(() => {
+                    $scope.loading = false;
+                    showInfoMsg("SAVED");
+                }).error(error => {
+                $scope.loading = false;
+                showWarningMsg("ERROR");
+                console.log(error);
+            });
+        };
 
-    $scope.setBannerForProductOfDay = () => {
-      $scope.loading = true;
-        console.log('updateBanners => ', $scope.bannerProductOfDay);
-      $http({
-        method: 'PUT',
-        url: '/api/banner/for/product',
-        data: $scope.bannerProductOfDay
-      })
-        .success(response => {
-          $scope.loading = false;
-          showInfoMsg("SAVED");
-        }).error(error => {
-        $scope.loading = false;
-        showWarningMsg("ERROR");
-      });
-    };
+        $scope.setBannerForShopBasket = () => {
+            $scope.loading = true;
+            $http({
+                method: 'PUT',
+                url: '/api/banner/in/basket',
+                data: $scope.bannerForShopBasket
+            })
+                .success(() => {
+                    $scope.loading = false;
+                    showInfoMsg("SAVED");
+                }).error(error => {
+                $scope.loading = false;
+                showWarningMsg("ERROR");
+                console.log(error);
+            });
+        };
 
-    $scope.setBannerForShopBasket = () => {
-      $scope.loading = true;
-        console.log('updateBanners => ', $scope.bannerForShopBasket);
-      $http({
-        method: 'PUT',
-        url: '/api/banner/in/basket',
-        data: $scope.bannerForShopBasket
-      })
-        .success(response => {
-          $scope.loading = false;
-          showInfoMsg("SAVED");
-        }).error(error => {
-        $scope.loading = false;
-        showWarningMsg("ERROR");
-      });
-    };
-
-
-
-    sideNavInit.sideNav();
-  }]);
+        sideNavInit.sideNav();
+    }]);
